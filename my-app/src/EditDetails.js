ea9a78be844e3c1bc0ca476fbddb28f4e6c31b67
Vismaya Wilson <vismayawilson3@gmail.com>
2022-02-12 13:20:06 +0530

    Edid issues resolved

--- my-app/src/EditDetails.js
+++ my-app/src/EditDetails.js
@@ -3,116 +3,170 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { formValidationSchema } from "./components/AddMovies";
+
+import { useFormikContext, Formik, Form, Field, useFormik } from "formik";
 
 export function EditDetails({ movieDetails, updateMovieDetails }) {
   const { id } = useParams();
   const [data, editData] = useState({ items: "" });
-  const textDetails = {
-    textAlign: "center",
-    color: "blue",
-    fontSize: "1.5rem",
-  };
 
   const updateDetails = () => {
     fetch("https://61fe505ba58a4e00173c97d5.mockapi.io/movies/" + id, {
       method: "GET",
     }).then((response) => {
-      response.json().then((mv) => editData({ items: mv.items }));
+      response.json().then((mv) => {
+        editData({ items: mv.items });
+        console.log("Edited Items: " + JSON.stringify(mv.items));
+      });
     });
   };
 
   useEffect(updateDetails, []);
   return (
     <div>
-      <h3 style={textDetails}>UPDATE MOVIE DETAILS {id}</h3>
-
-    {data.items?<EditMovieForm data={data} editData={editData} id={id} updateMovieDetails={updateMovieDetails}/>:null}
+      <h1>Get Data For Edit</h1>
+      {data.items ? (
+        <EditMovieForm
+          data={data}
+          editData={editData}
+          id={id}
+          updateMovieDetails={updateMovieDetails}
+        />
+      ) : null}
     </div>
   );
 }
 
-function EditMovieForm({data,editData,id}) {
-  const [name, updateName] = useState(data.items.name);
-  const [url, updateUrl] = useState(data.items.url);
-  const [rating, updateRating] = useState(data.items.rating);
-  const [summary, updateSummary] = useState(data.items.summary);
-  const [year, updateYear] = useState(data.year);
+function EditMovieForm({ data, editData, id }) {
   const history = useHistory();
+  const textDetails = {
+    textAlign: "center",
+    color: "blue",
+    fontSize: "1.5rem",
+  };
 
-  const updateApiCall=(data)=>{
+  const updateApiCall = (data) => {
+    console.log("AOI DATA",data);
     fetch("https://61fe505ba58a4e00173c97d5.mockapi.io/movies/" + id, {
       method: "PUT",
       body: JSON.stringify(data),
-      headers:{
-        "Content-type":"application/json"
-      }
+      headers: {
+        "Content-type": "application/json",
+      },
     }).then(() => {
-      history.push("/show");
-      // response.json().then((mv) => editData({ items: mv.items }));
+      // history.push("/show");
     });
-  }
+  };
+  const { handleSubmit, values, handleChange, handleBlur, touched, errors } =
+    useFormik({
+      initialValues: data.items,
+      validationSchema: formValidationSchema,
+      onSubmit: (values) => {
+        console.log("12222222", values);
+        updateApiCall(values);
+      },
+    });
   return (
-    <div>
-      <div>
-        <div className="add-movie-container">
-          <TextField
-            id="filled-basic"
-            label="Name"
-            defaultValue={data.items.name}
-            variant="filled"
-            onChange={(e) => updateName(e.target.value)}
-          />
-          <TextField
-            id="filled-basic"
-            label="Movie Url"
-            variant="filled"
-            defaultValue={data.items.poster}
-            onChange={(e) => updateUrl(e.target.value)}
-          />
-          <TextField
-            id="filled-basic"
-            label="Rating"
-            variant="filled"
-            defaultValue={data.items.rating}
-            onChange={(e) => updateRating(e.target.value)}
-          />
-          <TextField
-            id="filled-basic"
-            label="Summary"
-            variant="filled"
-            defaultValue={data.items.summary}
-            onChange={(e) => updateSummary(e.target.value)}
-          />
-          <TextField
-            id="filled-basic"
-            label="Release Year"
-            variant="filled"
-            defaultValue={data.items.releaseYear}
-            onChange={(e) => updateYear(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            onClick={() => {
-              const newMovie = {
-                id: data.id,
-                name: name,
-                poster: url,
-                rating: rating,
-                summary: summary,
-                releaseYear: year,
-              };
-              updateApiCall(newMovie);
-              // const copyOFMovieList = [...movieDetails];
-              // copyOFMovieList[id] = newMovie;
-              // console.log("1234", copyOFMovieList);
-              // updateMovieDetails(copyOFMovieList);
-              
-            }}
-          >
-            Save Changes
-          </Button>
-        </div>
+    // <div></div>
+    <form onSubmit={handleSubmit}>
+      <h3 style={textDetails}>UPDATE MOVIE DETAILS {id}</h3>
+      <div className="add-movie-container">
+        <TextField
+          id="filled-basic"
+          label="Name"
+          defaultValue={data.items.name}
+          variant="filled"
+          onChange={handleChange}
+          id="name"
+          name="name"
+          error={errors.name && touched.name}
+          helperText={errors.name && touched.name ? errors.name : ""}
+        />
+        <TextField
+          id="filled-basic"
+          label="Movie Url"
+          variant="filled"
+          defaultValue={data.items.poster}
+          id="poster"
+          name="poster"
+          onChange={handleChange}
+          error={errors.poster && touched.poster}
+          helperText={errors.poster && touched.poster ? errors.poster : ""}
+        />
+        <TextField
+          id="filled-basic"
+          label="Rating"
+          variant="filled"
+          defaultValue={data.items.rating}
+          id="rating"
+          name="rating"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={errors.rating && touched.rating}
+          helperText={errors.rating && touched.rating ? errors.rating : ""}
+        />
+        <TextField
+          id="filled-basic"
+          label="Summary"
+          variant="filled"
+          defaultValue={data.items.summary}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="summary"
+          name="summary"
+          error={errors.summary && touched.summary}
+          helperText={errors.summary && touched.summary ? errors.summary : ""}
+        />
+        <TextField
+          id="filled-basic"
+          label="Release Year"
+          variant="filled"
+          defaultValue={data.items.releaseYear}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="releaseYear"
+          name="releaseYear"
+          error={errors.releaseYear && touched.releaseYear}
+          helperText={
+            errors.releaseYear && touched.releaseYear ? errors.releaseYear : ""
+          }
+        />
+
+        <TextField
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="trailer"
+          name="trailer"
+          id="filled-basic"
+          label="Trailer"
+          value={values.trailer}
+          variant="filled"
+          error={errors.trailer && touched.trailer}
+          helperText={errors.trailer && touched.trailer ? errors.trailer : ""}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          onClick={() => {
+            // const newMovie = {
+            //   id: data.id,
+            //   name: name,
+            //   poster: url,
+            //   rating: rating,
+            //   summary: summary,
+            //   releaseYear: year,
+            // };
+            // updateApiCall(newMovie);
+            // const copyOFMovieList = [...movieDetails];
+            // copyOFMovieList[id] = newMovie;
+            // console.log("1234", copyOFMovieList);
+            // updateMovieDetails(copyOFMovieList);
+          }}
+        >
+          Save Changes
+        </Button>
       </div>
-    </div>
+    </form>
   );
 }
--- my-app/src/components/AddMovies.js
+++ my-app/src/components/AddMovies.js
@@ -2,14 +2,26 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import TextField from "@mui/material/TextField";
+import * as yup from "yup";
+import { validateYupSchema } from "formik";
+import { useFormik } from "formik";
+
+export const formValidationSchema = yup.object({
+  name: yup.string().required("Please fill name"),
+  releaseYear: yup.number().min(4,"Fill Year").required("Please fill Year"),
+  poster: yup.string().required("Please fill movie url"),
+  rating: yup.number().required("Please fill rating").min(0,"0 - 10 required").max(10,'Should not exceed 10'),
+  summary: yup.string().required("Please fill summary").min(20,"Minimum 20 character needed"),
+  trailer: yup.string().required("Please fill trailer").min(20,"Minimum 20 character needed"),
+});
 
 export function AddMovies() {
-  const [name, updateName] = useState("");
-  const [url, updateUrl] = useState("");
-  const [rating, updateRating] = useState("");
-  const [summary, updateSummary] = useState("");
-  const [year, updateYear] = useState("");
-  const [trailer, updateTrailer] = useState("");
+  // const [name, updateName] = useState("");
+  // const [url, updateUrl] = useState("");
+  // const [rating, updateRating] = useState("");
+  // const [summary, updateSummary] = useState("");
+  // const [year, updateYear] = useState("");
+  // const [trailer, updateTrailer] = useState("");
   const history = useHistory();
 
   const addData = (data) => {
@@ -21,70 +33,125 @@ export function AddMovies() {
       history.push("/show");
     });
   };
+ 
+  const { handleSubmit, values, handleChange, handleBlur, touched, errors } =
+    useFormik({
+      initialValues: {
+        name: "",
+        releaseYear: "",
+        poster: "",
+        rating: "",
+        summary: "",
+        trailer: "",
+      },
+      validationSchema: formValidationSchema,
+      onSubmit: (values) => {
+        console.log("12222222", values);addData(values);
 
+      },
+    });
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div className="add-movie-container">
         <TextField
           id="filled-basic"
           label="Name"
-          value={name}
+          value={values.name}
           variant="filled"
-          onChange={(e) => updateName(e.target.value)}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="name"
+          name="name"
+          error={errors.name && touched.name}
+          helperText={errors.name && touched.name? errors.name:""}
         />
+        
         <TextField
           id="filled-basic"
           label="Movie Url"
           variant="filled"
-          value={url}
-          onChange={(e) => updateUrl(e.target.value)}
+          value={values.poster}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="poster"
+          name="poster"
+          error={errors.poster && touched.poster}
+          helperText={errors.poster && touched.poster? errors.poster:""}
+         
         />
         <TextField
           id="filled-basic"
           label="Rating"
           variant="filled"
-          value={rating}
-          onChange={(e) => updateRating(e.target.value)}
+          value={values.rating}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="rating"
+          name="rating"
+          error={errors.rating && touched.rating}
+          helperText={errors.rating && touched.rating? errors.rating:""}
+        
         />
         <TextField
           id="filled-basic"
           label="Summary"
-          value={summary}
+          value={values.summary}
           variant="filled"
-          onChange={(e) => updateSummary(e.target.value)}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="summary"
+          name="summary"
+          error={errors.summary && touched.summary}
+          helperText={errors.summary && touched.summary? errors.summary:""}
+        
+        
+          
+         
         />
         <TextField
           id="filled-basic"
           label="Release Year"
           variant="filled"
-          value={year}
-          onChange={(e) => updateYear(e.target.value)}
+          value={values.year}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="releaseYear"
+          name="releaseYear"
+          error={errors.releaseYear && touched.releaseYear}
+          helperText={errors.releaseYear && touched.releaseYear? errors.releaseYear:""}
+         
         />
         <TextField
           id="filled-basic"
           label="Trailer"
-          value={trailer}
+          value={values.trailer}
           variant="filled"
-          onChange={(e) => updateTrailer(e.target.value)}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id="trailer"
+          name="trailer"
+          error={errors.trailer && touched.trailer}
+          helperText={errors.trailer && touched.trailer? errors.trailer:""}
         />
         <Button
+        type="submit"
           variant="contained"
           onClick={() => {
-            const newMovie = {
-              name: name,
-              poster: url,
-              rating: rating,
-              summary: summary,
-              releaseYear: year,
-              trailer: trailer,
-            };
+            // const newMovie = {
+            //   name: values.name,
+            //   poster: values.url,
+            //   rating: values.rating,
+            //   summary: values.summary,
+            //   releaseYear: values.year,
+            //   trailer: values.trailer,
+            // };
             // updateMovieDetails([...movieDetails, newMovie]);
-            addData(newMovie);
+            // addData(newMovie);
           }}
         >
           Add Movie
         </Button>
       </div>
-    </div>
+    </form>
   );
 }
